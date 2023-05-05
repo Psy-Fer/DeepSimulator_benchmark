f2143b687d78049f270e62f8f562c5384acb4633
James Ferguson <Psy-Fer@users.noreply.github.com>
2023-05-05 12:20:54 +1000

    Update download_and_install.sh

--- base_caller/guppy_3.1.5/download_and_install.sh
+++ base_caller/guppy_3.1.5/download_and_install.sh
@@ -1,11 +1,10 @@
 #-> ont-guppy-cpu
-if [ ! -d "ont-guppy-cpu" ]
-then
-	wget -q https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_3.1.5_linux64.tar.gz
-	tar xzf ont-guppy-cpu_3.1.5_linux64.tar.gz
-	rm -f ont-guppy-cpu_3.1.5_linux64.tar.gz
-fi
-
+#if [ ! -d "ont-guppy-cpu" ]
+#then
+#	wget -q https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_3.1.5_linux64.tar.gz
+#	tar xzf ont-guppy-cpu_3.1.5_linux64.tar.gz
+#	rm -f ont-guppy-cpu_3.1.5_linux64.tar.gz
+#fi
 #-> ont-guppy
 if [ ! -d "ont-guppy" ]
 then
